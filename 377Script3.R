96583ea35095d4e46bd8a138a42833769fe10e8f
ikebf <89275242+ikebf@users.noreply.github.com>
2021-08-24 09:40:13 -0400

    Update 377Script3.R

--- 377Script3.R
+++ 377Script3.R
@@ -26,3 +26,37 @@ reg = lm(wage ~ educ, data = wage1)
 
 #Then, use the summary() function
 summary(reg)
+
+####################
+
+#Problem set 1 examples. These won't give you the correct answers but will show 
+#you what commands to use. Remember that you can run lines of code by selecting them
+#and pressing Ctrl + Enter
+
+####################
+
+#Question 14
+mean(wage1$wage) #Calculate the mean wage in dataset wage1. You should change wage for educ
+
+#Question 15
+max(wage1$educ) - min(wage1$educ) #Calculate the range of educ in wage1. Again, change educ
+
+#Question 16
+ls(beauty) #View the variables in dataset beauty
+
+#Question 18
+cor(crime1$narr86, crime1$tottime) #This calculates the sample correlation between 
+#variables narr86 and tottime in the dataset crime1. Change this to get the right answer
+
+#Question 20 
+#No code, but remember that Var(X) = E(X^2) - E(X)^2
+
+#Question 21
+X = c(24,39,239,192,2) #Set a vector X. You'll need to change this
+var(X) #Calculates the sample variance of vector X
+
+#Question 22
+mean(X**2) - mean(X)**2 #This is how you could use the formula Var(X) = E(X^2) - E(X)^2
+#Remember that you'll have to define X correctly to get this to work
+
+
