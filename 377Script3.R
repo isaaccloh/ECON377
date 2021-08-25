0e7e857dc463ac5dec45d4a2e060b4399ddb6a57
ikebf <89275242+ikebf@users.noreply.github.com>
2021-08-25 14:14:02 -0400

    Update 377Script3.R

--- 377Script3.R
+++ 377Script3.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 
 # Set working directory
-setwd("C:/Users/lohi/OneDrive - UNC-Wilmington/Econometrics/R")
+setwd("C:/Users/lohi/OneDrive - UNC-Wilmington/Desktop/Fun times with metrics")
 
 #load Wooldridge package
 library(wooldridge)
@@ -52,7 +52,7 @@ cor(crime1$narr86, crime1$tottime) #This calculates the sample correlation betwe
 #No code, but remember that Var(X) = E(X^2) - E(X)^2
 
 #Question 21
-X = c(24,39,239,192,2) #Set a vector X. You'll need to change this
+X = c(12, 234, 12, 123, 2) #Set a vector X. You'll need to change this
 var(X) #Calculates the sample variance of vector X
 
 #Question 22
@@ -61,17 +61,17 @@ mean(X**2) - mean(X)**2 #This is how you could use the formula Var(X) = E(X^2) -
 
 ####################
 
-#OLS example: plots OLS regresion line and the line E[Y|X] that it's trying to estimate
+#OLS example: plots OLS regression line and the line E[Y|X] that it's trying to estimate
 
 ####################
 
-n = 100 #sample size
-sigma = 10 #Noise in the model
+n = 10000 #sample size
+sigma = 15 #Noise in the model
 beta0 = 2
 beta1 = 2
-X = rnorm(n)
-U = rnorm(n, sd = sigma)
-Y = beta0 + beta1 * X + U
+X = rnorm(n) #Randomly generate X
+U = rnorm(n, sd = sigma) #Randomly generate U
+Y = beta0 + beta1 * X + U #Set Y according to our econometric model
 plot(X, Y, main = "OLS regression line", xlab = "X", ylab = "Y")
 abline(lm(Y ~ X), col = "blue")
 curve(beta0 + beta1 * x, add = TRUE)
