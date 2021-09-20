d02bc3617b00807a7f2947b3769da17a7e624678
ikebf <89275242+ikebf@users.noreply.github.com>
2021-09-20 10:33:19 -0400

    Added script 8

---
+++ 377Script8.R
@@ -0,0 +1,82 @@
+#For 9/20/2021
+
+# Clear environment
+rm(list=ls())
+
+# Set working directory
+setwd("C:/Users/Isaac/OneDrive - UNC-Wilmington/Econometrics/R")
+
+#load Wooldridge package
+library(wooldridge)
+
+#regress wage on educ 
+reg2 = lm(wage ~ educ, data = wage1)
+
+#If you want a professional looking output:
+#install.packages('stargazer') #Uncomment this and do it once
+library(stargazer)
+stargazer('reg2', type = 'text')
+
+#Calculate hat(sigma)
+n = nrow(wage1)    #Set n equal to the number of observations from wage1
+ssr = sum(reg2$residuals^2)    #set ssr equal to sum of the squared residuals
+sigmahat = sqrt(1/(n-2)*ssr)    #using the formula for sigmahat
+
+#Calculate SSTX
+sstx = (n-1) * var(wage1$educ)    
+
+#Calculate se(hbeta1)
+se1 = sigmahat/sqrt(sstx)
+
+#Calculate the tvalue for hbeta1
+tval1 = reg2$coefficients[2]/se1
+
+#Getting the same information the easy way regressing log(wage) on education
+reg3 = lm(log(wage) ~ educ, data = wage1)
+summary(reg3)
+
+
+####################
+#Illustration of asymptotic distribution of OLS estimates
+#This time, dividing by the standard errors (look at how the distributions change with n)
+####################
+
+n = 10000 #Set sample size
+M = 100 #Number of experiments
+beta0 = 1
+beta1 = 1
+sigma = 2 #Standard deviation of U, i.e. sigma
+
+#Slope coefficient vector
+slopevec = rep(0,M)
+
+#Should we divide by standard errors?
+divide_option = FALSE
+
+#Monte Carlo
+
+for (i in 1:M) {
+  
+  #Generate data
+  Uvec = rnorm(n, mean = 0, sd = sigma)
+  Xvec = rnorm(n, mean = 0, sd = 2)
+  Yvec = beta0 + beta1 * Xvec + Uvec
+  
+  #Run regressions
+  reg = lm(Yvec ~ Xvec)
+  
+  se1 = sqrt(diag(vcov(reg)))[2]
+  
+  #Save slope coefficient
+  
+  if (divide_option) {
+    slopevec[i] = (reg$coefficients[2] - beta1) / se1 + beta1
+  } else {
+    slopevec[i] = reg$coefficients[2] 
+  }
+}
+
+hist(slopevec, main = paste("Histogram of 
+                            beta1 estimates with n =", 
+                            toString(n)),
+     xlim = c(beta1 - 2, beta1 + 2))
