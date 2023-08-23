e15848f99f78262bf6f6952ad85b37b5e4870a85
Isaac Loh <89275242+ikebf@users.noreply.github.com>
2023-08-23 12:11:40 -0400

    Add files via upload

---
+++ 377_2023/oct23.R
@@ -0,0 +1,20 @@
+# Comments tell us what we are doing
+
+# Printing hello world!
+print('Hello world!') # Hit ctrl + enter to run
+
+# Enter in our Taylor Swift data 
+mydata = c(0, 0, 6, 2, 1) # Saved the data as mydata
+
+# Take a sum
+0 + 0 + 6 + 2 + 1 # Caveman way
+sum(mydata) # Better way
+
+# Take a mean
+mean(mydata)
+
+# Take a median
+median(mydata)
+
+# If you need help
+?mean
