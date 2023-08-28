cba1383dda00e2da8ef6951485537250976e2635
Isaac Loh <89275242+ikebf@users.noreply.github.com>
2023-08-28 12:28:09 -0400

    Add files via upload

---
+++ 377_2023/aug28.R
@@ -0,0 +1,14 @@
+# Clear working memory
+rm(list = ls())
+
+# Example A.1
+beta_1 = 0.27
+delta_x = 200 
+beta_1 * delta_x
+
+# Example A.2
+beta_1 = -9.8
+beta_2 = 0.03
+delta_x1 = -2
+delta_x2 = 300
+beta_1 * delta_x1 + beta_2 * delta_x2
