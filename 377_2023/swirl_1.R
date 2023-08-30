718232187060e99df84e19b1a63d44e0af9e92b9
ikebf <89275242+ikebf@users.noreply.github.com>
2023-08-30 10:20:43 -0400

    Create swirl_1.R

---
+++ 377_2023/swirl_1.R
@@ -0,0 +1,84 @@
+## This is an example script that will handle the first swirl 
+## assignment. You should produce something like this .R file
+## and submit it to Canvas by Friday, 9/1/2023
+
+# Clear memory
+rm(list = ls())
+
+# Load swirl into memory
+library(swirl)
+
+# Beginning swirl
+swirl()
+
+# Username: last name, first initial
+lohi
+
+# Selecting the right course: 377 swirl
+1
+
+# Selecting the lesson (1: basic building blocks)
+1
+
+# Q1 
+5 + 7
+
+# Q2 
+x <- 5 + 7
+
+# Q3
+x
+
+# Q4
+y <- x - 3
+
+# Q5
+y
+
+# Q6
+z <- c(1.1,9,3.14)
+
+# Q7 
+?c
+
+# Q8
+z
+
+# Q9
+c(z, 555, z)
+
+# Q10
+z * 2 + 100
+
+# Q11
+my_sqrt <- sqrt(z-1)
+
+# Q12
+1
+
+# Q13
+my_sqrt
+
+# Q14
+my_div <- z / my_sqrt
+
+# Q15
+1
+
+# Q16
+my_div
+
+# Q17
+c(1,2,3,4) + c(0,10)
+
+# Q18
+c(1,2,3,4) + c(0,10,100)
+
+# Q19 
+z * 2 + 1000
+
+# Q20
+my_div
+
+# Submitting results
+1
