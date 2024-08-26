3fb93421652ae972c90cf2f4f0a06c4481cbbae0
Isaac Loh <89275242+isaaccloh@users.noreply.github.com>
2024-08-26 12:19:38 -0400

    Add files via upload

---
+++ 377_2024/aug_26.R
@@ -0,0 +1,19 @@
+# 8/26 
+# Ctrl + shift + enter runs everything!
+# Example A.1
+0.27 * (-100)
+beta1 = 0.27
+Deltax = 500
+beta1 * Deltax 
+
+# Example A.2
+# R way:
+Deltap = -2
+Deltai = 300 
+beta1 = -9.8
+beta2 = 0.03
+beta1 * Deltap + beta2 * Deltai
+
+# Calculator way:
+-9.8 * (-2) + 0.03 * (300)
+
