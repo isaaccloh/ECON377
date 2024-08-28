d07a74e3ad783ea0c2435896f753fd2adab9fdae
Isaac Loh <89275242+isaaccloh@users.noreply.github.com>
2024-08-28 12:28:37 -0400

    Add files via upload

---
+++ 377_2024/aug_28.R
@@ -0,0 +1,18 @@
+# August 28
+# Percentage change
+xnew = 47
+xold = 44
+(xnew - xold) / xold * 100
+
+# Sample variance/standard deviation
+df = c(7,9,8)
+var(df) # Variance
+sd(df) # Standard deviation
+
+# Installing swirl
+install.packages("swirl")
+
+# Do this every time you load R
+library(swirl) # Loads swirl package
+
+install_course_github("ikebf", "377_swirl") # Installing 377 swirl course
