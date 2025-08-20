4c1dcfd5bd1ce4cb487ae6761f6cd16b0e904389
Isaac Loh <89275242+isaaccloh@users.noreply.github.com>
2025-08-20 12:16:47 -0400

    Add files via upload

---
+++ 377_2025/aug_20.R
@@ -0,0 +1,22 @@
+# This is a comment. This documents what we're doing.
+# This is a R script. This is where we will be coding
+1 + 1 # Press ctrl + enter to run a line of code (on macs, cmd + enter)
+
+# R commands 
+# To enter in a list of numbers (vectors):
+c(0, 0, 0, 6, 0)
+
+# To save as e.g. ts_vec
+ts_vec = c(0, 0, 0, 6, 0)
+
+# To view ts_vec
+print(ts_vec)
+
+# To take the sum
+sum(ts_vec)
+
+# To take the mean
+mean(ts_vec)
+
+# To get length of vector
+length(ts_vec)
